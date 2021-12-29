5055847a2fba1fe3f8b31d76bb48c0cdc3a2cfe8
vanakkam-nanba <karthikeyank.19eee@kongu.edu>
2021-12-29 21:05:34 +0530

    Add files via upload

---
+++ modules/index.sh
@@ -0,0 +1,29 @@
+url=$1
+
+printf "==============================================\n" >> /app/results/$url-output.txt
+printf "=======  FINAL RECON REPORT OF TARGET  =======\n" >> /app/results/$url-output.txt
+printf "==============================================\n\n" >> /app/results/$url-output.txt
+
+printf "[CTRL+F AND SEARCH THE REQUIRED RESULT]\n\n" >> /app/results/$url-output.txt
+printf "==== INDEX OF REPORT [$url] ==== \n\n" >> /app/results/$url-output.txt
+
+printf "1  - DNSCAN\n" >> /app/results/$url-output.txt
+printf "2  - CLICKJACKING\n" >> /app/results/$url-output.txt
+printf "3  - CORSTEST\n" >> /app/results/$url-output.txt
+printf "4  - FIREWALL DETECTION\n" >> /app/results/$url-output.txt
+printf "5  - WEBDAV FILE UPLOAD TEST\n" >> /app/results/$url-output.txt
+printf "6  - ROBOTS.TXT CHECKER\n" >> /app/results/$url-output.txt
+printf "7  - SUBDOMAINS\n" >> /app/results/$url-output.txt
+printf "8  - DIRECTORY BRUTE\n" >> /app/results/$url-output.txt
+printf "9  - JS FINDER + LINK FINDER\n" >> /app/results/$url-output.txt
+printf "10 - SUBDOMAINS TAKEOVER CHECK\n" >> /app/results/$url-output.txt
+printf "11 - SUBDOMAINS [TITLE + CNAME]\n" >> /app/results/$url-output.txt
+printf "12 - SUBDOMAINS [IP + WEBSERVER]\n" >> /app/results/$url-output.txt
+printf "13 - WHOIS LOOKUP\n" >> /app/results/$url-output.txt
+printf "14 - SECURITY HEADERS CHECK\n" >> /app/results/$url-output.txt
+printf "15 - WEB TECHNOLOGIES\n" >> /app/results/$url-output.txt
+
+printf "\n\n\n" >> /app/results/$url-output.txt
+printf "##########################################################################################\n" >> /app/results/$url-output.txt
+printf "##########################################################################################" >> /app/results/$url-output.txt
+printf "\n\n\n" >> /app/results/$url-output.txt
---
+++ retrieve.py
@@ -0,0 +1,26 @@
+# python retrive.py {url}
+
+import os
+import psycopg2
+import base64
+import sys
+
+url = sys.argv[1]
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
+
+cur.execute(f"select result from output where domain = '{url}'")
+t = cur.fetchall()
+
+res = t[0][0]
+final = base64.standard_b64decode(res)
+final = final.decode('utf-8')
+f = open('/app/results/{}-output.txt'.format(url), 'w')
+f.write(final)
+f.close()
+
+conn.commit()
+cur.close()
+conn.close()
