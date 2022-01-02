db75cd63b6eee966a1a9a53b1c235a61b224f5c1
vanakkam-nanba <karthikeyank.19eee@kongu.edu>
2022-01-02 13:13:41 +0530

    Update gf_patterns.sh

--- modules/gf_patterns.sh
+++ modules/gf_patterns.sh
@@ -1,4 +1,3 @@
-#gf_patterns
 
 echo "17) GF PATTERNS" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
@@ -8,7 +7,7 @@ url=$1
 mkdir /app/gf
 for i in $(/app/modules/binaries/gf -list)
 do
- /app/modules/binaries/gf $i /app/final_urls | tee /app/gf/$i-$url
+ /app/modules/binaries/gf $i /app/results/$url-gau.txt | tee /app/gf/$i-$url
  if [ -s /app/gf/$i-$url ] then
    printf "GF PATTERN : $i : \n\n" >> /app/results/$url-output.txt
    cat /app/gf/$i-$url | tee -a /app/results/$url-output.txt
