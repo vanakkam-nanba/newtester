59ebbed88f8f8f915d689cb43978b8a63d5f8149
vanakkam-nanba <karthikeyank.19eee@kongu.edu>
2022-01-02 13:11:15 +0530

    Update gather_urls.sh

--- modules/gather_urls.sh
+++ modules/gather_urls.sh
@@ -1,3 +1,4 @@
+
 #urls gather
 
 url=$1
@@ -6,14 +7,13 @@ echo "16) GATHERING ALL URLS" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
 
 /app/modules/binaries/gau $url | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
-/app/modules/binaries/gospider -t 150 -s http://$url -d 4 | grep "\[url\]" | cut -d " " -f 5 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
+/app/modules/binaries/gospider -t 80 -s http://$url -d 4 | grep "\[url\]" | cut -d " " -f 5 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
 /app/modules/binaries/hakrawler -plain -url http://$url -depth 4 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
 
-cat /app/urls | /app/modules/binaries/qsreplace -a | tee /app/final_urls
+cat /app/urls | qsreplace -a | tee /app/results/$url-gau.txt
 
-printf "Gathered all urls for the $url successfully !" >> /app/results/$url-output.txt
-
-# goto /gau/{url} to download the gau_urls file
+printf "Gathered all urls for the $url successfully !\n" >> /app/results/$url-output.txt
+printf "Goto /gau/{url} to view all the gathered urls | Download the file for your future recon" >> /app/results/$url-output.txt
 
 printf "\n\n\n" >> /app/results/$url-output.txt
 printf "##########################################################################################\n" >> /app/results/$url-output.txt
