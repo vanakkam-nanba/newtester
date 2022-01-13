9bbeacb3841d834e61f4cca4a6d125d3b3e864a5
vanakkam-nanba <karthikeyank.19eee@kongu.edu>
2022-01-14 00:34:35 +0530

    Update gather_urls.sh

--- modules/gather_urls.sh
+++ modules/gather_urls.sh
@@ -6,7 +6,7 @@ url=$1
 echo "16) GATHERING ALL URLS" >> /app/results/$url-output.txt
 printf "\n\n" >> /app/results/$url-output.txt
 
-/app/modules/binaries/gau $url | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
+/app/modules/binaries/gospider -t 20 -s http://$url -d 2 | grep "\[url\]" | cut -d " " -f 5 | egrep -v '(.pdf|.css|.png|.jpeg|.jpg|.svg|.gif|.wolf)' | tee -a /app/urls
 
 cat /app/urls | /app/modules/binaries/qsreplace -a | tee /app/results/$url-gau.txt
 
