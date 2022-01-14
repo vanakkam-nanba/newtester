900d77ce2d3996be1a0545a5e14533fdd1cc52d2
vanakkam-nanba <karthikeyank.19eee@kongu.edu>
2022-01-14 11:47:41 +0530

    Update run.py

--- run.py
+++ run.py
@@ -61,12 +61,6 @@ os.system(f"bash modules/shcheck.sh {url}")
 #15_wappalyzer_cli
 os.system(f"bash modules/wappy.sh {url}")
 
-#16_urls_gather
-os.system(f"bash modules/gather_urls.sh {url}")
-
-#17_seperating_with_gf_patterns
-os.system(f"bash modules/gf_patterns.sh {url}")
-
 #18
 
 ###########################################################
